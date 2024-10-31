dfe196960c1651aeb93932e4d6ab405c47c9a226
Nick Pnevmatikatos <nickpnevmati@gmail.com>
2024-10-31 21:02:27 +0200

    Added laptop config

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -10,7 +10,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nuclear-desktop";
+  networking.hostName = "nuclear-laptop";
   networking.networkmanager.enable = true;
   networking.firewall = {
     enable = true;
