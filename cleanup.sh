b6484a2e95005c840ea93a8822ba5d84ef60e06f
Nick Pnevmatikatos <nickpnevmati@gmail.com>
2025-02-10 14:47:31 +0200

    Fixed cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,2 @@
-sudo nix-env --delete-generations --profile /nix/var/nix/profiles/system 5d
 sudo nix-collect-garbage
-sudo /run/current-system/bin/switch-to-configuration boot
+sudo nix-collect-garbage -d
