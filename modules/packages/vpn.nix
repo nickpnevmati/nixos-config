70f048ea02e0a539e7505d8816174742da9b63fd
Nick Pnevmati <nickpnevmati@gmail.com>
2025-12-18 00:59:18 +0200

    Updated vpn.nix

--- modules/packages/vpn.nix
+++ modules/packages/vpn.nix
@@ -1,9 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    openvpn
-    networkmanager-vpnc
-    networkmanager-openvpn
-  ];
+  # environment.systemPackages = with pkgs; [
+  #   openvpn
+  #   networkmanager-vpnc
+  #   networkmanager-openvpn
+  # ];
+
+  networking.networkmanager.plugins = [ pkgs.networkmanager-openvpn ];
 }
